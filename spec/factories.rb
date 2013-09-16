ffd38516573a8e83e3fc3fe09716ce33299f5dfc
Azubuike Motanya <acmotanya@gmail.com>
2013-09-16 16:51:56 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Azubuike Motanya"
+    email    "acmotanya@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
