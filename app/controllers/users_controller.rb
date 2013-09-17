f4c1c8f6f70dc2ea605de72bfcdd11275e24a81b
Azubuike Motanya <acmotanya@gmail.com>
2013-09-17 15:16:23 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Pipe Bang Post!"
       redirect_to @user
     else
@@ -18,7 +19,6 @@ class UsersController < ApplicationController
     end
   end
 
-  
   private
   
     def user_params
